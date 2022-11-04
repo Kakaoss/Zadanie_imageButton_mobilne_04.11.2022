55823369b4aabf0df53ffac0c2384cb2ab946585
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-04 16:22:28 +0100

    skonczone

--- app/src/main/java/com/barstool/zadanie_imagebutton_mobilne_04112022/MainActivity.kt
+++ app/src/main/java/com/barstool/zadanie_imagebutton_mobilne_04112022/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.graphics.alpha
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -17,7 +19,52 @@ class MainActivity : AppCompatActivity() {
         val przycisk3 = findViewById<ImageButton>(R.id.obrazek3)
         val przycisk4 = findViewById<ImageButton>(R.id.obrazek4)
         val przycisk5 = findViewById<ImageButton>(R.id.obrazek5)
-
-
+        przycisk1.setOnClickListener {
+            if (przycisk1.alpha.equals(0.5F)){
+                przycisk1.alpha = 1F
+            }
+            else{
+                przycisk1.alpha = 0.5F
+            }
+            Toast.makeText(baseContext, "kot", Toast.LENGTH_SHORT).show()
+        }
+        przycisk2.setOnClickListener {
+            if (przycisk1.alpha.equals(0.5F)){
+                przycisk1.alpha = 1F
+            }
+            else{
+                przycisk1.alpha = 0.5F
+            }
+            Toast.makeText(baseContext, "krowa", Toast.LENGTH_SHORT).show()
+        }
+        przycisk1.setOnClickListener {
+            if (przycisk1.alpha.equals(0.5F)){
+                przycisk1.alpha = 1F
+            }
+            else{
+                przycisk1.alpha = 0.5F
+            }
+            Toast.makeText(baseContext, "pies", Toast.LENGTH_SHORT).show()
+        }
+        przycisk1.setOnClickListener {
+            if (przycisk1.alpha.equals(0.5F)){
+                przycisk1.alpha = 1F
+            }
+            else{
+                przycisk1.alpha = 0.5F
+            }
+            Toast.makeText(baseContext, "strus", Toast.LENGTH_SHORT).show()
+        }
+        przycisk5.setOnClickListener {
+            if (przycisk1.alpha.equals(0.5F)){
+                przycisk1.alpha = 1F
+            }
+            else{
+                przycisk1.alpha = 0.5F
+            }
+            Toast.makeText(baseContext, "tygrys", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
